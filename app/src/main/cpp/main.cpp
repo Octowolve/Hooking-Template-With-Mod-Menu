d0c36f2faeb92a868a4ceb84b8f269c79f9f5454
Octowolve <32366759+z3r0Sec@users.noreply.github.com>
2020-03-19 01:43:18 +0100

    Update main.cpp

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -18,6 +18,7 @@ void GameManager_LateUpdate(void *instance) {
     //Check if instance is NULL to prevent crashes!  If the instance object is NULL,
     //this is what the call to update would look like in C++:
     //NULL.Update(); and dat doesnt make sense right?
+    //Also check if our example boolean is true so the hack will work then. if not it just returns the old method
     if(instance != NULL && exampleBooleanForToggle) {
         if(!GameManagerLateUpdateHookInitialized){
             //Check if this hook initialized. If so log
