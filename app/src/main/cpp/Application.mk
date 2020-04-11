2bb31d0d7af6f39e83176f564ffd3a2f780bbe6a
Octowolve <32366759+z3r0Sec@users.noreply.github.com>
2020-04-11 02:08:11 +0200

    Update Application.mk

--- app/src/main/cpp/Application.mk
+++ app/src/main/cpp/Application.mk
@@ -12,4 +12,5 @@ ifneq ($(APP_OPTIM), debug)
   APP_LDFLAGS += -Wl,--strip-all
   APP_CFLAGS  += -fvisibility=hidden -fvisibility-inlines-hidden
   APP_CFLAGS  += -g0 -O3 -fomit-frame-pointer -ffunction-sections -fdata-sections
+  APP_CPPFLAGS += -std=c++11
 endif
