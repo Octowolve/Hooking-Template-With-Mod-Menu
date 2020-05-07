80a9f615291edf09997e311edec8061be4552c04
Octo-kun <32366759+Octowolve@users.noreply.github.com>
2020-05-07 18:24:52 +0200

    Update Utils.h

--- app/src/main/cpp/Includes/Utils.h
+++ app/src/main/cpp/Includes/Utils.h
@@ -9,6 +9,7 @@ static DWORD libBase;
 DWORD findLibrary(const char *library);
 DWORD getAbsoluteAddress(const char* libraryName, DWORD relativeAddr);
 bool isLibraryLoaded(const char *libraryName);
+void MakeToast(JNIEnv* env, jobject thiz);
 
 DWORD findLibrary(const char *library) {
     char filename[0xFF] = {0},
@@ -61,4 +62,32 @@ bool isLibraryLoaded(const char *libraryName) {
     }
     return false;
 }
+
+void MakeToast(JNIEnv* env, jobject thiz){
+    //Add our toast in here so it wont be easy to change by simply editing the smali and cant
+    //be cut out because this method is needed to start the hack (I'm smart)
+    jstring jstr = env->NewStringUTF("Mod by Octowolve."); //Edit this text to your desired toast message!
+    jclass toast = env->FindClass("android/widget/Toast");
+    jmethodID methodMakeText =
+            env->GetStaticMethodID(
+                    toast,
+                    "makeText",
+                    "(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;");
+    if (methodMakeText == NULL) {
+        LOGE("toast.makeText not Found");
+        return;
+    }
+    //The last int is the length on how long the toast should be displayed
+    //0 = Short, 1 = Long
+    jobject toastobj = env->CallStaticObjectMethod(toast, methodMakeText,
+                                                   thiz, jstr, 0);
+
+    jmethodID methodShow = env->GetMethodID(toast, "show", "()V");
+    if (methodShow == NULL) {
+        LOGE("toast.show not Found");
+        return;
+    }
+    env->CallVoidMethod(toastobj, methodShow);
+}
+
 #endif
