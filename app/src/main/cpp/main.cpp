c3611fe48a8284dc69325073c55fce6f2292e826
Octo-kun <32366759+Octowolve@users.noreply.github.com>
2020-05-08 00:35:02 +0200

    Update main.cpp

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -125,6 +125,7 @@ void changeEditText(JNIEnv *env, jclass clazz, jint i, jstring value){
     return;
 }
 
+extern "C"
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
     JNIEnv* env;
     if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
